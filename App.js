9d883beeb68aeaeb0f46e215ca4c8a367808e6c7
Evelin Maripuu <evelin.marit@gmail.com>
2025-05-23 12:29:47 +0300

    Making Items Deletable and Using ID-s, p 31-33

--- App.js
+++ App.js
@@ -4,41 +4,54 @@ import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals(currentCourseGoals => [
-      ...currentCourseGoals, 
-      {text: enteredGoalText, id: Math.random().toString()},
-    ]);
-  };
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	function addGoalHandler(enteredGoalText) {
+		setCourseGoals((currentCourseGoals) => [
+			...currentCourseGoals,
+			{ text: enteredGoalText, id: Math.random().toString() },
+		]);
+	}
+
+	function deleteGoalHandler(id) {
+		setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
+	}
 
 	return (
 		<View style={styles.appContainer}>
 			<GoalInput onAddGoal={addGoalHandler} />
 
-      <View style={styles.goalsContainer}>      
-      <FlatList 
-        data={courseGoals} 
-        renderItem={(itemData) => {
-        return <GoalItem text={itemData.item.text} />;
-      }}
-      keyExtractor={(item, index) => {
-        return item.id;
-      }} 
-      alwaysBounceVertical={false} />
-      </View>
+			<View style={styles.goalsContainer}>
+				<FlatList
+					data={courseGoals}
+					renderItem={(itemData) => {
+						return (
+							<GoalItem
+								text={itemData.item.text}
+                id={itemData.item.id}
+								onDeleteItem={deleteGoalHandler}
+							/>
+						);
+					}}
+					keyExtractor={(item, index) => {
+						return item.id;
+					}}
+					alwaysBounceVertical={false}
+				/>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16
-  },
-  goalsContainer: {
-    flex: 5
-  }  
+		flex: 1,
+		paddingTop: 50,
+		paddingHorizontal: 16,
+	},
+	goalsContainer: {
+		flex: 5,
+	},
 });
